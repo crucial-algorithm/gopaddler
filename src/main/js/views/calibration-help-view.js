b8bf7a514a7777833e79b41dd86d8c1a8eaba8d8
Joaquim Rego <kimi_rego@hotmail.com>
2016-03-27 20:30:13 +0100

    Implementation of full onboarding flow - Added missing file

---
+++ src/main/js/views/calibration-help-view.js
@@ -0,0 +1,40 @@
+'use strict';
+
+
+function CalibrationHelpView(page, context, request) {
+    request = request || {};
+
+    var $page = $(page)
+        , $content = $page.find('[data-selector="slick"]')
+        , $gotIt
+        , isStartSession = !!(request.from === 'start-session');
+
+
+    // adjust slideshow for when the user reaches it from start session or from help
+    if (isStartSession) {
+        $page.find('[data-step="3"]').find('.got-it').remove();
+    } else {
+        $page.find('[data-step="4"]').remove();
+    }
+
+    $gotIt = $page.find('.got-it');
+
+    setTimeout(function () {
+        $content.slick({
+            dots: true,
+            speed: 300,
+            infinite: false,
+            arrows: false
+        });
+    }, 0);
+
+    $gotIt.on('tap', function () {
+        if (context.preferences().isShowCalibrationTips()) {
+            context.preferences().calibrationTipsShown();
+        }
+        if (isStartSession) context.navigate('calibration', true, {from: "start-session"});
+        else App.back();
+    });
+}
+
+exports.CalibrationHelpView = CalibrationHelpView;

5cfb80f6a2f31613beab700116b360690529d870
Joaquim Rego <kimi_rego@hotmail.com>
2016-10-10 10:24:58 +0100

    Move hook definition to config.xml; Removed app.js and app.js.map from repository

--- src/cordova/ios/plist.js
+++ src/cordova/ios/plist.js
@@ -1,19 +1,20 @@
 #!/usr/local/bin/node
 
-var fs    = require('fs');
+var fs = require('fs');
 var plist = require('plist');
 
-var FILEPATH = 'platforms/ios/GoPaddler/GoPaddler-Info.plist';
+var FILEPATH = 'platforms/ios/Paddler/Paddler-Info.plist';
 
-module.exports = function (context) {
-
-
+function overridePlist(context) {
     var xml = fs.readFileSync(FILEPATH, 'utf8');
     var obj = plist.parse(xml);
 
     obj.UIStatusBarHidden = true;
     obj.UIViewControllerBasedStatusBarAppearance = false;
+    obj.CFBundleDisplayName = "Paddler";
 
     xml = plist.build(obj);
-    fs.writeFileSync(FILEPATH, xml, { encoding: 'utf8' });
-};
+    fs.writeFileSync(FILEPATH, xml, {encoding: 'utf8'});
+}
+
+module.exports = overridePlist;

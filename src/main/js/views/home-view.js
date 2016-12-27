6524482995eba4109b16abfd6154914adcc79a41
Joaquim Rego <kimi_rego@hotmail.com>
2016-12-27 23:51:08 +0000

    Increment version to 0.8.6

--- src/main/js/views/home-view.js
+++ src/main/js/views/home-view.js
@@ -53,7 +53,7 @@ function HomeView(page, context, request) {
         manufacturer: device.manufacturer,
         isVirtual: device.isVirtual,
         serial: device.serial,
-        paddler: "0.8.5"
+        paddler: "0.8.6"
     });
 
 

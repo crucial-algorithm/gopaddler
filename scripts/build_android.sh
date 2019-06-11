5ab69c229aa6d99749ea54e2dcf7fb12f4379dd2
Filipe <filipe@gopaddler.com>
2019-06-11 09:34:04 +0100

    Add missing -- in command line arguments (indicates that those are platform specific arguments)

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cordova build android --release --storePassword=$ANDROID_KEY_STORE_PASSWORD --password=$ANDROID_KEY_PASSWORD
+cordova build android --release -- --storePassword=$ANDROID_KEY_STORE_PASSWORD --password=$ANDROID_KEY_PASSWORD

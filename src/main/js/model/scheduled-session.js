a95e4e69330deadef1403bdc755dc5e41071b01a
Filipe <filipe@gopaddler.com>
2020-07-13 15:30:35 +0100

    Change smples sessions for cycling

--- src/main/js/model/scheduled-session.js
+++ src/main/js/model/scheduled-session.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import Api from '../server/api';
+import AppSettings from "../utils/app-settings";
 
 /**
  * @typedef {Object} SplitDefinition
@@ -31,16 +32,22 @@ class ScheduledSession {
         window.localStorage.setItem("scheduled-sessions", JSON.stringify(result));
     }
 
+    /**
+     *
+     * @param isDev
+     * @return {Array<ScheduledSession>}
+     */
     static load(isDev = false) {
+        let list = JSON.parse(window.localStorage.getItem("scheduled-sessions"))
+
         if (isDev === true) {
-            return [...DEV_EXPRESSIONS];
+            list = [...DEV_EXPRESSIONS];
         }
         if (Api.User.hasCoach() === false) {
-            return [...SAMPLE_EXPRESSIONS];
+            list = ScheduledSession.sampleSessions();
         }
-        let list = JSON.parse(window.localStorage.getItem("scheduled-sessions"));
         if (!list)
-            return undefined;
+            return [];
 
         let result = [], s;
         for (let json of list) {
@@ -56,7 +63,7 @@ class ScheduledSession {
         let deferred = $.Deferred();
 
         if (!Api.User.hasCoach()) {
-            deferred.resolve([...SAMPLE_EXPRESSIONS]);
+            deferred.resolve(ScheduledSession.sampleSessions());
             return deferred.promise();
         }
 
@@ -76,6 +83,16 @@ class ScheduledSession {
         });
 
         return deferred.promise();
+    }
+
+    static sampleSessions() {
+        let sessions = [];
+        AppSettings.switch(() => {
+            sessions = [...SAMPLE_CANOEING_EXPRESSIONS]
+        }, () => {
+            sessions = [...SAMPLE_CYCLING_EXPRESSIONS]
+        })
+        return sessions;
     }
 
     loadFromServerResponse(json) {
@@ -135,10 +152,37 @@ class ScheduledSession {
 }
 
 // expressions for PROD
-const SAMPLE_EXPRESSIONS = [
+const SAMPLE_CANOEING_EXPRESSIONS = [
+    {
+        id: "KpBuBNb5NGqcA4QxJ",
+        date: new Date(Date.now() - 86400000),
+        expression: "4 x 5'/7’",
+        splits: [
+            {_duration: 5, _recovery: false, _unit: 'minutes'}, {_duration: 7, _recovery: true, _unit: 'minutes'},
+            {_duration: 5, _recovery: false, _unit: 'minutes'}, {_duration: 7, _recovery: true, _unit: 'minutes'},
+            {_duration: 5, _recovery: false, _unit: 'minutes'}, {_duration: 7, _recovery: true, _unit: 'minutes'},
+            {_duration: 5, _recovery: false, _unit: 'minutes'}, {_duration: 7, _recovery: true, _unit: 'minutes'},
+            {_duration: 5, _recovery: false, _unit: 'minutes'}]
+    },
+
+    {
+        id: "5cTfZPtBoTxMTzmSs",
+        expression: "2 x 1000m/8’ + 2 x 750m/8’",
+        date: new Date(Date.now() - 2 * 86400000),
+        splits: [
+            {_duration: 1000, _recovery: false, _unit: 'meters'}, {_duration: 8, _recovery: true, _unit: 'minutes'},
+            {_duration: 1000, _recovery: false, _unit: 'meters'}, {_duration: 8, _recovery: true, _unit: 'minutes'},
+            {_duration: 750, _recovery: false, _unit: 'meters'}, {_duration: 8, _recovery: true, _unit: 'minutes'},
+            {_duration: 750, _recovery: false, _unit: 'meters'}, {_duration: 8, _recovery: true, _unit: 'minutes'}
+        ]
+    }
+];
+
+const SAMPLE_CYCLING_EXPRESSIONS = [
     {
         id: "Hiyc4CZTX4ZGWLsFh",
         expression: "5 x (6’/1’ + 2’)/4’",
+        date: new Date(Date.now() - 86400000),
         splits: [
             {_duration: 6, _recovery: false, _unit: 'minutes'}, {_duration: 1, _recovery: true, _unit: 'minutes'},
             {_duration: 2, _recovery: false, _unit: 'minutes'}, {_duration: 4, _recovery: true, _unit: 'minutes'},
@@ -154,12 +198,13 @@ const SAMPLE_EXPRESSIONS = [
 
     {
         id: "DCdaSuSEtji6Emivf",
-        expression: "2 x 1000m/8’ + 2 x 750m/8’",
+        expression: "2 x 4000m/8’ + 2 x 2000m/8’",
+        date: new Date(Date.now() - 2 * 86400000),
         splits: [
-            {_duration: 1000, _recovery: false, _unit: 'meters'}, {_duration: 8, _recovery: true, _unit: 'minutes'},
-            {_duration: 1000, _recovery: false, _unit: 'meters'}, {_duration: 8, _recovery: true, _unit: 'minutes'},
-            {_duration: 750, _recovery: false, _unit: 'meters'}, {_duration: 8, _recovery: true, _unit: 'minutes'},
-            {_duration: 750, _recovery: false, _unit: 'meters'}, {_duration: 8, _recovery: true, _unit: 'minutes'}
+            {_duration: 4000, _recovery: false, _unit: 'meters'}, {_duration: 8, _recovery: true, _unit: 'minutes'},
+            {_duration: 4000, _recovery: false, _unit: 'meters'}, {_duration: 8, _recovery: true, _unit: 'minutes'},
+            {_duration: 2000, _recovery: false, _unit: 'meters'}, {_duration: 8, _recovery: true, _unit: 'minutes'},
+            {_duration: 2000, _recovery: false, _unit: 'meters'}, {_duration: 8, _recovery: true, _unit: 'minutes'}
         ]
     }
 ];
@@ -169,13 +214,13 @@ const DEV_EXPRESSIONS = [
         expression: "20''/10'' + 20''/10''",
         id: 1,
         splits: [{_duration: 20, _recovery: false, _unit: 'seconds'}, {_duration: 10, _recovery: true, _unit: 'seconds'},{_duration: 20, _recovery: false, _unit: 'seconds'}],
-        date: moment().add(-3, 'd')
+        date: new Date(Date.now() - 86400000),
     },
     {
         expression: "50m/10'' + 50m/10'",
         id: 1,
         splits: [{_duration: 100, _recovery: false, _unit: 'meters'}, {_duration: 20, _recovery: true, _unit: 'seconds'},{_duration: 100, _recovery: false, _unit: 'meters'}],
-        date: moment().add(-2, 'd')
+        date: new Date(Date.now() - 2 * 86400000),
     }
 ];
 
